4b55be44211fd41e6a184e030b63f73c7280a536
Chrizelda Moises Oria <chrizelda.moises.oria@hitachivantara.com>
2019-10-22 22:00:47 +0800

    modularize all the logic inside gilded rose class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,53 +1,95 @@
+require 'delegate'
+
 class GildedRose
+  
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+     ItemWrapper.wrap(item)
+    end 
+  end
+
+end  #end of Class GildedRose
+    
+class ItemWrapper < SimpleDelegator
+   def self.wrap(item)
+    case item.name
+    when "Aged Brie"
+      AgedBrie.new(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      BackstagePass.new(item)
+    when "Conjured Mana Cake"
+      ConjuredItem.new(item)
+    when "Sulfuras, Hand of Ragnaros"
+      SulfurasItem.new(item)
+    else
+      new(item)
     end
+
+    update(item)
+  end
+
+  def self.update(item)
+    return if item.name == "Sulfuras, Hand of Ragnaros"
+    age(item)
+    update_quality(item)
+  end
+
+  def self.age(item)
+    item.sell_in -= 1
+  end
+  
+  def self.update_quality(item)
+    item.quality += caluculate_quality_adjustment(item)
+  end  
+
+  def self.caluculate_quality_adjustment(item)
+    adjustment = 0
+
+    if item.sell_in < 0
+      adjustment -= 1
+    else
+      adjustment -= 1
+    end
+
+    adjustment
+  end
+   
+  def self.quality=(new_quality)
+    new_quality = 0 if new_quality < 0
+    new_quality = 50 if new_quality > 50
+    super(new_quality)
+  end
+
+end
+
+class AgedBrie < ItemWrapper
+  def caluculate_quality_adjustment
+    adjustment = 1
+    if sell_in < 0
+      adjustment += 1
+    end
+
+    adjustment
+  end
+end
+
+class SulfurasItem < ItemWrapper
+  def caluculate_quality_adjustment
+    #This item don't change anything
+  end
+end
+
+class ConjuredItem < ItemWrapper
+  def caluculate_quality_adjustment
+    adjustment = -2
+    if sell_in < 0
+      adjustment -= 2
+    end
+
+    adjustment
   end
 end
